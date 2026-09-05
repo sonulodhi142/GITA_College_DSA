147a3276093891bea9f6e8f8b15a2ed433b037ae
Sonu Lodhi <sp95759201@gmail.com>
2026-09-05 09:48:22 +0530

    1 to n and n to 1

--- AIML_2/stack/factorial.c
+++ AIML_2/stack/factorial.c
@@ -10,11 +10,29 @@ int factorail(int n){
 }
 
 // print N to 1 using recursion. if n = 5 : output => 5 4 3 2 1
+void print_N_to_1(int n){
+    if(n == 0){
+        return;
+    }
+    printf("%d ", n);
+    print_N_to_1(n-1);
+}
+
+// print 1 to n using recursion. if n = 5 : output => 1 2 3 4 5
+void print_1_to_N(int n){
+    if(n == 0){
+        return;
+    }
+    print_1_to_N(n-1);
+    printf("%d ", n);
+}
 
 int main(){
     int n;
     printf("Enter the value : ");
     scanf("%d", &n);
-    printf("factorial of %d : %d\n", n, factorail(n));
+    print_1_to_N(n);
+    // print_N_to_1(n);
+    // printf("factorial of %d : %d\n", n, factorail(n));
     return 0;
 }
