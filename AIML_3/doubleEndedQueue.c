dc5bef888759a2cb0e553d9b0de5754309f065cd
Sonu Lodhi <sp95759201@gmail.com>
2026-09-09 12:29:00 +0530

    double ended Queue

---
+++ AIML_3/doubleEndedQueue.c
@@ -0,0 +1,125 @@
+#include<stdio.h>
+#define max 5
+
+int d_queue[max];
+int front = -1;
+int rear = -1;
+
+// function to check queue is empty or not
+int isEmpty(){
+    return front == -1;
+}
+
+// function to check queue is full or not
+int isFull(){
+    return (rear + 1) % max == front;
+}
+
+// function to insert at rear
+void insertRear(int n){
+    if(isFull()){
+        printf("\nQueue overflow\n");
+    }
+    else{
+        if(isEmpty()){
+            front = rear = 0;
+        }
+        else if(rear == max-1){
+            rear = 0;
+        }
+        else{
+            rear++;
+        }
+        d_queue[rear] = n;
+        printf("\n%d inserted at rear\n", n);
+    }
+}
+
+// function to insert fornt
+void insertFront(int n){
+    if(isFull()){
+        printf("\nQueue overflow\n");
+    }
+    else{
+        if(isEmpty()){
+            front = rear = 0;
+        }
+        else if(front == 0){
+            front = max-1;
+        }
+        else{
+            front--;
+        }
+        d_queue[front] = n;
+        printf("\n%d inseted at front\n");
+    }
+}
+
+// function to delete rear
+void deleteRear(){
+    if(isEmpty()){
+        printf("\nQueue underflow\n");
+    }
+    else{
+        int n = d_queue[rear];
+        if(rear == 0){
+            rear = max-1;
+        }
+        else if(rear == front){
+            rear = front = -1;
+        }
+        else{
+            rear--;
+        }
+        printf("\n%d deleted from rear\n", n);
+    }
+}
+
+// function to delete front
+void deleteFront(){
+    if(isEmpty()){
+        printf("\nQueue underflow\n");
+    }
+    else{
+        int n = d_queue[front];
+        if(front == max-1){
+            front = 0;
+        }
+        else if(front == rear){
+            front = rear = -1;
+        }
+        else{
+            front++;
+        }
+        printf("\n%d deleted from front\n", n);
+    }
+}
+
+// function to display all element of queue
+void display(){
+    if(isEmpty()){
+        printf("\nQueue underflow\n");
+    }
+    else{
+        printf("\nQueue : ");
+        int i = front;
+        while(1){
+            printf("%d  ", d_queue[i]);
+
+            if(i == rear){
+                break;
+            }
+            i = (i+1)%max;
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    insertFront(8);
+    insertRear(9);
+    insertFront(0);
+    deleteRear();
+    deleteFront();
+    display();
+}
