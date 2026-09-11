c237cbf84bf99a0a3316d23de2c12b97793a1e23
Sonu Lodhi <sp95759201@gmail.com>
2026-09-11 12:36:56 +0530

    circular queue

---
+++ AIML_2/Queue/circularQueue.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#define max 5
+
+int c_queue[max];
+int front = -1;
+int rear = -2;
+
+// function to check queue is empty or not
+int isEmpty(){
+    return front == -1;
+}
+
+// function to check queue is full or not
+int isFull(){
+    return (front + 1) % max == rear;
+}
+
+// function to insert element 
+void enqueue(int n){
+    if(isFull()){
+        printf("\nQueue is overflow\n");
+    }
+    else{
+        if(isEmpty()){
+            front = rear = 0;
+        }
+        else{
+            rear = (rear + 1)%max;
+        }
+        c_queue[rear] = n;
+        printf("\n%d inserted\n", n);
+    }
+}
+
+// function to delete front
+void dequeue(){
+    if(isEmpty()){
+        printf("\nQueue underflow\n");
+    }
+    else{
+        int x = c_queue[front];
+        if(front == rear){
+            front = rear = -1;
+        }
+        else{
+            front = (front + 1)%max;
+        }
+        printf("\n%d deleted\n", x);
+    }
+}
