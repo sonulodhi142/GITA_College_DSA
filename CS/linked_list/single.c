c322011a03104682b4fd9dc7d4c149ce82085252
Sonu Lodhi <sp95759201@gmail.com>
2026-09-21 12:42:33 +0530

    linked list

--- CS/linked_list/single.c
+++ CS/linked_list/single.c
@@ -49,14 +49,108 @@ void insert_at_end(int data){
     struct node* newNode=createNode(data);
     if(head==NULL){
         head = newNode;
-
     }
-}    
+    else{
+        struct node *temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+    printf("\n inserted at end\n", data);
+}
+
+// function to delete head
+void delete_from_begin(){
+    if(head == NULL){
+        printf("\nList is empty");
+    }
+    else{
+        struct node *temp = head;
+        head = head->next;
+        printf("\n%d is deleted from head\n", temp->data);
+        free(temp);
+    }
+}
+
+// function to delete end
+void delete_from_end(){
+    if(head == NULL){
+        printf("\nList is empty");
+    }
+    else{
+        struct node *temp = head;
+        struct node *delete = NULL;
+        while (temp->next->next != NULL){
+            temp = temp->next;
+        }
+        delete = temp->next;
+        temp->next = NULL;
+        printf("\n%d deleted form end\n", delete->data);
+        free(delete);
+    }
+}
+
+// function to search element
+void search(int target){
+    if(head == NULL){
+        printf("\nList is empty.\n");
+    }
+    else{
+        struct node *temp = head;
+        int i = 1;
+        while(temp != NULL){
+            if(temp->data == target){
+                printf("\n%d is found at %d position\n", target, i);
+                return;
+            }
+            i++;
+            temp = temp->next;
+        }
+        printf("\nElement id not found\n");
+    }
+}
+
+
 int main(){
- insert_at_begin(50);
-    insert_at_begin(60);
-    insert_at_begin(70);
-    display();
-    
-    return 0;
+    int value, option;
+    while(1){
+        printf("\n===========single linked list operations=======\n\n");
+        printf("1. insert begin.\n");
+        printf("2. insert end.\n");
+        printf("3. delete begin.\n");
+        printf("4. delete end.\n");
+        printf("5. diplay.\n");
+        printf("6. search.\n");
+        printf("7. exit program.\n");
+
+        printf("\nEnter option : ");
+        scanf("%d", &option);
+
+        switch (option)
+        {
+        case 1:
+            printf("Enter value : ");
+            scanf("%d", &value);
+            insert_at_begin(value);
+            break;
+        case 2:
+            printf("Enter value : ");
+            scanf("%d", &value);
+            insert_at_end(value);
+            break;
+        case 3: delete_from_begin(); break;
+        case 4: delete_from_end(); break;
+        case 5: display(); break;
+        case 6: 
+            printf("\nEnter value to search : ");
+            scanf("%d", &value);
+            search(value);
+            break;
+        case 7: printf("\nProgram exit successfully\n"); return 0;
+        default:
+            printf("\ninvailed option");
+            break;
+        }
+    }
 }
