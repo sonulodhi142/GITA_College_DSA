01eb3345a3170e0a3c1344c4ad821e133aebebd3
Sonu Lodhi <sp95759201@gmail.com>
2026-09-23 09:58:29 +0530

    circular linked list

--- AIML_4/linked_list/circular.c
+++ AIML_4/linked_list/circular.c
@@ -54,11 +54,134 @@ void display(){
     printf("head\n");
 }
 
+// function to insert at end
+void insert_at_end(int data){
+    struct node *newNode = createNode(data);
+
+    if(head == NULL){
+        head = newNode;
+        newNode->next = head;
+    }
+    else{
+        struct node *temp = head;
+        while(temp->next != head){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->next = head;
+    }
+    printf("\n%d inserted at end\n", data);
+}
+
+// function to delete head
+void delete_from_begin(){
+    if(head == NULL){
+        printf("\nList is empty\n");
+        return;
+    }
+    struct node *delete = head;
+    struct node *temp = head;
+    if(head == head->next){
+        head = NULL;
+    }
+    else{
+        while(temp->next != head){
+            temp = temp->next;
+        }
+        head = head->next;
+        temp->next = head;
+    }
+    printf("\n%d deleted from begin\n", delete->data);
+    free(delete);
+}
+
+// function to delete from end
+void delete_from_end(){
+    if(head == NULL){
+        printf("\nList is empty\n");
+        return;
+    }
+    struct node *delete = head;
+    struct node *temp = head;
+    if(head == head->next){
+        head = NULL;
+    }
+    else{
+        while(temp->next->next != head){
+            temp = temp->next;
+        }
+        delete = temp->next;
+        temp->next = head;
+    }
+    printf("\n%d deleted from end\n", delete->data);
+    free(delete);
+}
+
+// function search element 
+void search(int target){
+    if(head == NULL){
+        printf("\nlist is empty\n");
+        return;
+    }
+    struct node *temp = head;
+    int i = 1;
+    do{
+        if(temp->data == target){
+            printf("\n%d is found at %d position\n",target, i);
+            return;
+        }
+        i++;
+        temp = temp->next;
+    }while(temp != head);
+    printf("\nElement is not found\n");
+}
+
 int main(){
-    insert_at_begin(60);
-    insert_at_begin(60);
-    insert_at_begin(60);
-    insert_at_begin(60);
-    insert_at_begin(60);
-    display();
+  int option, value, pos;
+  while(1){
+    printf("\n==== Circular linked list =======\n\n");
+    printf("1. insert at begin.\n");
+    printf("2. insert at end\n");
+    printf("3. delete from begin.\n");
+    printf("4. delete from end\n");
+    printf("5. search element\n");
+    printf("6. display\n");
+    printf("7. exit programe\n");
+
+    printf("\nEnter option : ");
+    scanf("%d", &option);
+
+    switch(option){
+        case 1:
+            printf("Enter value: ");
+            scanf("%d", &value);
+            insert_at_begin(value);
+            break;
+        case 2:
+            printf("Enter value: ");
+            scanf("%d", &value);         // github.com/sonulodhi142/GITA_College_DSA
+
+            insert_at_end(value);
+            break;
+        case 3:
+            delete_from_begin();
+            break;
+        case 4:
+            delete_from_end();
+            break;
+        case 5:
+            printf("Enter value to search : ");
+            scanf("%d", &value);
+            search(value);
+            break;
+        case 6:
+            display();
+            break;
+        case 7:
+            printf("\nProgram terminated successfully\n");
+            return 0;
+        default:
+            printf("\ninvailed option\n");
+    }
+  }
 }
